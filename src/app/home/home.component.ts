6de4ef715c895fd613a5607ee1b688c1b49858a4
Vitor Moutim <moutimg@gmail.com>
2024-03-21 09:15:23 -0300

    feat: create link for button in home to login

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TokenAuthService } from '../shared/token-auth.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  redirectURL: string = '';
 
+  constructor(private tokenService: TokenAuthService) { }
+
+  ngOnInit() {
+    this.redirectURL = this.tokenService.redirectURL;
+  }
 }
