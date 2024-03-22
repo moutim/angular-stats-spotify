7e0f2e93d267a282190f0d8e58cd2cac64617fae
Vitor Moutim <moutimg@gmail.com>
2024-03-22 15:28:30 -0300

    fix

--- src/app/shared/token-auth.service.ts
+++ src/app/shared/token-auth.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import variables from '../../../variables';
-const { CLIENT_ID, REDIRECT_URL, CLIENT_SECRET } = process.env;
 import { ActivatedRoute } from '@angular/router';
 
 @Injectable({
