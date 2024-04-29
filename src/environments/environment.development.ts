e785fd46551f654c616e77cb224897b1b7b70e6e
Vitor Moutim <moutimg@gmail.com>
2024-04-29 14:06:34 -0300

    feat: remove valores do enviroment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  CLIENT_ID: 'ad5577aaca644405975f331123d60a58',
-  CLIENT_SECRET: 'd3041563f8674d8bba717d1dd94faf1b',
-  REDIRECT_URL: 'http://localhost:4200/login',
+  CLIENT_ID: '',
+  CLIENT_SECRET: '',
+  REDIRECT_URL: '',
 };
